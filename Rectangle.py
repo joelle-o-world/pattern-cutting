c7b71056a2e10f9a43a4496a05d8a83937c35aa5
Joelle Plow <joelleplow@pm.me>
2022-10-11 00:06:33 +0100

    Enlargement of rectangles

--- Rectangle.py
+++ Rectangle.py
@@ -13,11 +13,20 @@ class Rectangle:
     def height(self):
         return self.top - self.bottom
 
+    def enlarge(self, margin):
+        "Create a new enlarged rectangle with margin added to all sides"
+        return Rectangle(
+                top = self.top + margin,
+                bottom = self.bottom - margin,
+                left = self.left - margin,
+                right = self.right + margin
+            )
+
 def minimumBoundingRect(objects) -> Rectangle:
     "Find the minimum bounds rectangle of many objects"
     return Rectangle(
-        top = max([object.top for object in objects]),
-        bottom = min([object.bottom for object in objects]),
-        left = min([object.left for object in objects]),
-        right = max([object.right for object in objects])
-    )
+            top = max([object.top for object in objects]),
+            bottom = min([object.bottom for object in objects]),
+            left = min([object.left for object in objects]),
+            right = max([object.right for object in objects])
+        )
