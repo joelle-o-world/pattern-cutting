f5f5183059ea7f87d7213e72206088ab74f86455
Joelle Plow <joelleplow@pm.me>
2022-10-11 00:04:04 +0100

    add simple rectangle class
    we need rectangles

---
+++ Rectangle.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    def __init__(self, left, top, right, bottom):
+        self.left = left
+        self. top = top
+        self.right = right
+        self.bottom = bottom
+
+    @property
+    def width(self):
+        return self.right - self.left
+
+    @property
+    def height(self):
+        return self.top - self.bottom
+
+def minimumBoundingRect(objects) -> Rectangle:
+    "Find the minimum bounds rectangle of many objects"
+    return Rectangle(
+        top = max([object.top for object in objects]),
+        bottom = min([object.bottom for object in objects]),
+        left = min([object.left for object in objects]),
+        right = max([object.right for object in objects])
+    )
