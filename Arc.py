2b2502743604046d1fa0bf39ef32065110a4815a
Joelle Plow <joelleplow@pm.me>
2022-10-13 23:54:41 +0100

    Arc

---
+++ Arc.py
@@ -0,0 +1,25 @@
+from vec2 import vec2
+from PolyLine import PolyLine
+import numpy as np
+import math
+from render import render
+
+
+class Arc:
+    def __init__(self, center: vec2, radius: float, startAngle: float, endAngle: float):
+        self.center = center
+        self.radius = radius
+        self.startAngle = startAngle
+        self.endAngle = endAngle
+
+    def pointAtAngle(self, angle):
+        return self.center + vec2(1, 0).withAngle(angle).withLength(self.radius)
+
+    def polyline(self, resolution = 100):
+        step = (self.endAngle - self.startAngle) / resolution
+        return PolyLine([self.pointAtAngle(angle) for angle in np.arange(self.startAngle, self.endAngle, step)])
+
+
+if __name__ == "__main__":
+    myArc = Arc(center = vec2(0, 50), radius=20, startAngle = 0, endAngle = math.pi)
+    render([myArc.polyline()]).saveSvg("Arc example.svg")
