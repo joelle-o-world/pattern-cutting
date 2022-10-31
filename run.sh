6c73423e599bd0cf61ed2e8fa3f3a57fa05d021e
Joelle Plow <joelleplow@pm.me>
2022-10-31 14:02:26 +0000

    Stop bringing browser to forground

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ pandoc readme.src.md -o readme.ipynb
 jupyter nbconvert readme.ipynb --execute --to markdown 
 rm readme.ipynb
 pandoc -s readme.md --css style.css --css https://gist.githubusercontent.com/dashed/6714393/raw/ae966d9d0806eb1e24462d88082a0264438adc50/github-pandoc.css -o readme.html
-open readme.html
+open readme.html -g
