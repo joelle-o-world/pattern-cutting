29d106ef257aa1685179f965ea32c21fff07923a
Joelle Plow <joelleplow@pm.me>
2022-12-13 23:00:24 +0000

    Add Ve's sizes

---
+++ velvet-caveat.py
@@ -0,0 +1,17 @@
+from src.sizing.BodyMeasurements import BodyMeasurements
+
+# Measurements for Velvet Caveat
+
+velvet_caveat = BodyMeasurements(
+    chest=400,
+    bust=950,
+    front_shoulder_to_waist=480,
+    waist=790,
+    neck_size=390,
+    nape_to_waist=450,
+    bust_to_waist=240,
+    under_bust=87,
+    shoulders=480,
+)
+
+print(velvet_caveat)
