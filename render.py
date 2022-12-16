75ecb6cff97ecf4d8fdc6330d9fad5ce7519d2b5
Joelle Plow <joelleplow@pm.me>
2022-12-16 19:10:05 +0000

    Add A4 grid to all renders

--- render.py
+++ render.py
@@ -1,10 +1,14 @@
 import drawSvg as draw
 import numpy as np
+import string
 
 from src.geometry.Rectangle import minimumBoundingRect
 from src.geometry.Shape import Shape
 from src.geometry.Vector import Vector
 
+def n2a(n,b=string.ascii_uppercase):
+   d, m = divmod(n,len(b))
+   return n2a(d-1,b)+b[m] if d else b[m]
 
 def render(*objects):
     "Quickly render any number of objects as SVG"
@@ -22,12 +26,9 @@ def render(*objects):
         fill="none",
     )
 
-    # Draw horizontal lines
+    # Draw horizontal A4 lines
     gridColor = "#dddddd"
-    gridSize = 50
-    i = 0
-    for y in np.arange(rect.bottom, rect.top, gridSize):
-        dasharray = None if i % 5 == 0 else 5
+    for y in np.arange(rect.bottom, rect.top, 297):
         d.append(
             draw.Line(
                 rect.left,
@@ -35,13 +36,11 @@ def render(*objects):
                 rect.right,
                 y,
                 stroke=gridColor,
-                stroke_dasharray=dasharray,
             )
         )
-        i += 1
-    i = 0
-    for x in np.arange(rect.left, rect.right, gridSize):
-        dasharray = None if i % 5 == 0 else 5
+
+    # Draw vertical A4 lines
+    for x in np.arange(rect.left, rect.right, 210):
         d.append(
             draw.Line(
                 x,
@@ -49,10 +48,52 @@ def render(*objects):
                 x,
                 rect.top,
                 stroke=gridColor,
-                stroke_dasharray=dasharray,
             )
         )
-        i += 1
+
+    # Draw A4 labels
+    col = 0
+    for x in np.arange(rect.left + 210/2, rect.right, 210):
+        row = 0
+        for y in np.arange(rect.bottom + 297/2, rect.top, 297):
+            label = "{}{}".format(n2a(col), row)
+            d.append(
+                    draw.Text(label, 10, x, y, stroke="none", fill=gridColor)
+                    )
+            
+
+            row += 1
+        col += 1
+
+
+
+    gridSize = 50
+    gridColor = "#ffcccc66"
+    # Draw horizontal grid lines
+    for y in np.arange(rect.bottom, rect.top, gridSize):
+        d.append(
+            draw.Line(
+                rect.left,
+                y,
+                rect.right,
+                y,
+                stroke=gridColor,
+                stroke_dasharray=5
+            )
+        )
+
+    # Draw vertical A4 lines
+    for x in np.arange(rect.left, rect.right, gridSize):
+        d.append(
+            draw.Line(
+                x,
+                rect.bottom,
+                x,
+                rect.top,
+                stroke=gridColor,
+                stroke_dasharray=5
+            )
+        )
 
     for object in objects:
         d.append(object.svg())
