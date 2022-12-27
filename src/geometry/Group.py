7016d9d5bc3b29faddc04464863d6a902492a0a3
Joelle Plow <joelleplow@pm.me>
2022-12-27 19:06:29 +0000

    Groups use a dictionary to store objects

--- src/geometry/Group.py
+++ src/geometry/Group.py
@@ -7,26 +7,38 @@ class Group:
     label = None
 
     def __init__(self, *objects):
-        self.objects = [*objects]
+        self.objects = {}
+        for object in objects:
+            self.append(object)
+
+    def new_unused_key(self, prefix="unlabeled_"):
+        i = 0
+        while "{}{}".format(prefix, i) in self.objects:
+            i += 1
+        return "{}{}".format(prefix, i)
 
     def append(self, obj):
-        self.objects.append(obj)
+        self.objects[self.new_unused_key()] = obj
+
+    def iterate_objects(self):
+        for key in self.objects:
+            yield self.objects[key]
 
     @property
     def left(self) -> float:
-        return min([obj.left for obj in self.objects])
+        return min([obj.left for obj in self.iterate_objects()])
 
     @property
     def right(self) -> float:
-        return max([obj.right for obj in self.objects])
+        return max([obj.right for obj in self.iterate_objects()])
 
     @property
     def top(self) -> float:
-        return max([obj.top for obj in self.objects])
+        return max([obj.top for obj in self.iterate_objects()])
 
     @property
     def bottom(self) -> float:
-        return min([obj.bottom for obj in self.objects])
+        return min([obj.bottom for obj in self.iterate_objects()])
 
     @property
     def width(self):
@@ -37,9 +49,9 @@ class Group:
         return self.top - self.bottom
 
     def svg(self):
-        return svg.Group([obj.svg() for obj in self.objects])
+        return svg.Group([obj.svg() for obj in self.iterate_objects()])
 
     def move(self, x: float, y: float):
         return Group(
-            *[obj.move(x, y) if isMovable(obj) else obj for obj in self.objects]
+            *[obj.move(x, y) if isMovable(obj) else obj for obj in self.iterate_objects()]
         )
