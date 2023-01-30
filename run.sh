14f2437edee9cee823698366d1cb532db5a28c37
Joelle Plow <joelleplow@pm.me>
2023-01-30 11:49:53 +0000

    Use `poetry run` instead of depend on `poetry shell`

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf readme_files
 mkdir readme_files
 pandoc readme.src.md -o readme.ipynb
-jupyter nbconvert readme.ipynb --execute --to markdown
+poetry run jupyter nbconvert readme.ipynb --execute --to markdown
 rm readme.ipynb
 pandoc -s --toc --metadata title="Pattern Cutting Python Library" readme.md --css style.css --css https://gist.githubusercontent.com/dashed/6714393/raw/ae966d9d0806eb1e24462d88082a0264438adc50/github-pandoc.css -o readme.html
 
