068a68af841a7e644b9f3b5b42a5daa326ab04f7
Joelle Plow <joelleplow@pm.me>
2023-04-11 18:44:41 +0100

    Add Tskeny hand measurements

---
+++ src/sizing/tskenya.py
@@ -0,0 +1,10 @@
+from src.sizing.BodyMeasurements import BodyMeasurements
+from src.units import inch
+
+
+tskenya = BodyMeasurements(
+    palm_circumference=9 * inch,
+    hand_circumference=10 * inch,
+    wrist=9 * inch,
+    elbow_circumference=14 * inch,
+)
